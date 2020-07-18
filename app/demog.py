6ce5a790d07ebe847c7b76903d1be4ee138fe1a6
Phil Hunt <cabalamat@gmail.com>
2020-07-18 23:43:51 +0100

    /demographics now saves

--- app/demog.py
+++ app/demog.py
@@ -1,12 +1,14 @@
 # demog.py = demographics page
 
+from flask import request, redirect
 
 from allpages import app, jinjaEnv
 from bozen.butil import pr, prn
 
+import ht
 import config
 import models
-from permission import needUser
+from permission import needUser, currentUserName
 
 prn("*** front.py ***")
 
@@ -15,11 +17,20 @@ prn("*** front.py ***")
 @app.route('/demographics', methods=['POST', 'GET'])
 @needUser
 def demographics():
-    ud = models.getUserDemographics("")
+    ud = models.getUserDemographics(currentUserName())
+    msg = ""
+        
+    if request.method=='POST':
+        ud = ud.populateFromRequest(request)
+        if ud.isValid():
+            ud.save()
+            msg = "Saved user demographics"
+    #//if    
     
     tem = jinjaEnv.get_template("demographics.html")
     h = tem.render(
         ud = ud,
+        msg = ht.goodMessageBox(msg),
     )
     return h
 
